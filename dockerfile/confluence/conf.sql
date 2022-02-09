8a398d886e814a4e6fbe3fb25d4d777d50557533
ZhangShY <Zhang_shengyao11@126.com>
2022-02-10 00:30:03 +0800

    update conf mysql

--- dockerfile/confluence/conf.sql
+++ dockerfile/confluence/conf.sql
@@ -3,4 +3,7 @@ CREATE DATABASE confluence CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 grant all on confluence.* to 'confuser'@'%';
 
+SET GLOBAL tx_isolation='READ-COMMITTED';
+
 jdbc:mysql://127.0.0.1:3306/confluence?useUnicode=true&characterEncoding=utf8
+
